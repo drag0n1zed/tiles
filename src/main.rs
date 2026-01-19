9389b8944b31dfc16227a8083b32e18e50ef19e5
drag <drag0n1zed@outlook.com>
2026-01-19 23:01:37 +0800

    refactor to (y, x), save/load .ron, basic TUI

--- src/main.rs
+++ src/main.rs
@@ -1,164 +1,115 @@
+mod objects;
+
 use std::{
     error::Error,
-    fs::{File, write},
-    io::BufReader,
+    fs::{read_to_string, write},
+    time::{Duration, Instant},
 };
 
-use ndarray::prelude::*;
-use ron::ser::PrettyConfig;
-use serde::{Deserialize, Serialize};
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 
-#[derive(Clone, Copy)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, Default)]
-enum Tile {
-    #[default]
-    Empty,
-    Blocker,
-    Regular(u32), // color
-}
-
-#[derive(Serialize, Deserialize)]
-struct Grid {
-    #[serde(skip)]
-    data: Array2<Tile>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct VecGrid {
-    dim: [usize; 2],
-    pattern: Vec<Vec<Tile>>,
-}
-
-impl Grid {
-    fn new(width: usize, length: usize) -> Self {
-        Self {
-            data: Array2::from_elem((width, length), Tile::Empty),
-        }
-    }
-
-    fn get_width(&self) -> usize {
-        self.data.dim().0
-    }
-
-    fn get_length(&self) -> usize {
-        self.data.dim().1
-    }
-
-    fn get(&self, x: usize, y: usize) -> Option<&Tile> {
-        self.data.get((x, y))
-    }
-
-    fn set(&mut self, x: usize, y: usize, tile: Tile) -> bool {
-        match self.data.get_mut((x, y)) {
-            Some(val) => {
-                *val = tile;
-                true
-            }
-            None => false,
-        }
-    }
-
-    // Iterate order: iterate through every column from top to bottom, from left to right
-    // [0, 0] ... [0, l - 1],
-    // [1, 0] ... [1, l - 1],
-    // [2, 0] ... [2, l - 1],
-    // ...
-    // [w - 1, 0] ... [w - 1, l - 1],
-    fn move_grid(&mut self, direction: Direction) {
-        match direction {
-            Direction::Left => {
-                for x in 0..self.get_width() {
-                    for y in 0..self.get_length() {
-                        self.move_tile(x, y, direction);
-                    }
-                }
-            }
-            Direction::Right => {
-                for x in (0..self.get_width()).rev() {
-                    for y in 0..self.get_length() {
-                        self.move_tile(x, y, direction);
-                    }
-                }
-            }
-            Direction::Up => {
-                for y in 0..self.get_length() {
-                    for x in 0..self.get_width() {
-                        self.move_tile(x, y, direction);
-                    }
-                }
-            }
-            Direction::Down => {
-                for y in (0..self.get_length()).rev() {
-                    for x in 0..self.get_width() {
-                        self.move_tile(x, y, direction);
-                    }
-                }
-            }
-        };
-    }
-
-    fn move_tile(&mut self, x: usize, y: usize, direction: Direction) {
-        let (tx, ty) = match direction {
-            Direction::Left => (x.wrapping_sub(1), y),  // (x - 1, y)
-            Direction::Right => (x.wrapping_add(1), y), // (x + 1, y)
-            Direction::Up => (x, y.wrapping_sub(1)),    // (x, y + 1)
-            Direction::Down => (x, y.wrapping_add(1)),  // (x, y - 1)
-        };
-
-        let from = self.data.get((x, y)).unwrap();
-        let Some(to) = self.data.get((tx, ty)) else {
-            // Hit the wall
-            return;
-        };
-
-        // Target is regular or blocker tile: cannot move
-        // Origin is empty or blocker tile: cannot move
-        if let (Tile::Regular(_), Tile::Empty) = (from, to) {
-            self.data.swap((x, y), (tx, ty));
-        }
-    }
-
-    fn to_vec(&self) -> VecGrid {
-        VecGrid {
-            dim: [self.get_width(), self.get_length()],
-            pattern: self
-                .data
-                .columns()
-                .into_iter()
-                .map(|row| row.iter().copied().collect())
-                .collect(),
-        }
-    }
-
-    fn from_vec(format: VecGrid) -> Result<Self, Box<dyn Error>> {
-        let vec: Vec<Tile> = format.pattern.into_iter().flatten().collect();
-        let data = Array2::from_shape_vec((format.dim[0], format.dim[1]), vec)?;
-        Ok(Self { data })
-    }
-}
+use crate::objects::{Direction, Grid};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut grid = Grid::new(3, 3);
-    grid.set(2, 0, Tile::Regular(0));
-    grid.set(2, 1, Tile::Regular(1));
-    // let file = File::open("grid.json")?;
-    // let reader = BufReader::new(file);
-    // let vec: VecGrid = serde_json::from_reader(reader)?;
-    // let mut grid = Grid::from_vec(vec)?;
+    // Import save file
+    let save_file = read_to_string("grid.ron")?;
+    let mut grid = Grid::from_ron(save_file.as_str())?;
 
-    grid.move_grid(Direction::Left);
+    let grid = ratatui::run(|terminal| {
+        let mut app = App::from_grid(grid);
+        app.run(terminal)?;
+        let result: Result<Grid, Box<dyn Error>> = Ok(app.into_grid());
+        result
+    })?;
 
-    let pretty_config = PrettyConfig::new().depth_limit(2);
-    let ron = ron::ser::to_string_pretty(&grid.to_vec(), pretty_config)?;
-    println!("{}", ron);
-    write("grid.ron", ron)?;
+    // Export save file
+    let save_file = grid.to_ron();
+    println!("{}", save_file);
+    write("grid.ron", save_file)?;
 
     Ok(())
+}
+
+struct App {
+    grid: Grid,
+    exit: bool,
+}
+
+impl App {
+    fn new(width: usize, length: usize) -> Self {
+        Self {
+            grid: Grid::new(width, length),
+            exit: false,
+        }
+    }
+
+    fn from_grid(grid: Grid) -> Self {
+        Self { grid, exit: false }
+    }
+
+    fn into_grid(self) -> Grid {
+        self.grid
+    }
+
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn Error>> {
+        let tick_rate = Duration::from_millis(100);
+        let mut last_tick = Instant::now();
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+
+            if event::poll(timeout)? {
+                match event::read()? {
+                    Event::Key(key) => self.handle_key_press(key),
+                    _ => (),
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_key_press(&mut self, key: event::KeyEvent) {
+        match key.code {
+            KeyCode::Left => self.grid.move_grid(Direction::Left),
+            KeyCode::Right => self.grid.move_grid(Direction::Right),
+            KeyCode::Up => self.grid.move_grid(Direction::Up),
+            KeyCode::Down => self.grid.move_grid(Direction::Down),
+            KeyCode::Esc => self.exit = true,
+            _ => {}
+        }
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" TILES ".bold());
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .border_set(border::THICK);
+
+        let text = Text::from(vec![Line::from(vec!["Hi".into()])]);
+
+        Paragraph::new(text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
---
+++ src/objects.rs
@@ -0,0 +1,155 @@
+use std::error::Error;
+
+use ndarray::prelude::*;
+use ron::ser::PrettyConfig;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize, Default)]
+pub enum Tile {
+    #[default]
+    Empty,
+    Blocker,
+    Regular(u32), // color
+}
+
+pub struct Grid {
+    data: Array2<Tile>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct VecGrid {
+    length: usize,
+    width: usize,
+    pattern: Vec<Vec<Tile>>,
+}
+
+impl Grid {
+    pub fn new(length: usize, width: usize) -> Self {
+        Self {
+            data: Array2::from_elem((length, width), Tile::Empty),
+        }
+    }
+
+    pub fn get_array(&self) -> &Array2<Tile> {
+        &self.data
+    }
+
+    pub fn get_length(&self) -> usize {
+        self.data.dim().0
+    }
+
+    pub fn get_width(&self) -> usize {
+        self.data.dim().1
+    }
+
+    pub fn get(&self, y: usize, x: usize) -> Option<&Tile> {
+        self.data.get((y, x))
+    }
+
+    pub fn set(&mut self, y: usize, x: usize, tile: Tile) -> bool {
+        match self.data.get_mut((y, x)) {
+            Some(val) => {
+                *val = tile;
+                true
+            }
+            None => false,
+        }
+    }
+
+    // Iterate order: iterate through every column from top to bottom, from left to right
+    // [0, 0] ... [0, l - 1],
+    // [1, 0] ... [1, l - 1],
+    // [2, 0] ... [2, l - 1],
+    // ...
+    // [w - 1, 0] ... [w - 1, l - 1],
+    pub fn move_grid(&mut self, direction: Direction) {
+        match direction {
+            Direction::Left => {
+                for x in 0..self.get_width() {
+                    for y in 0..self.get_length() {
+                        self.move_tile(y, x, direction);
+                    }
+                }
+            }
+            Direction::Right => {
+                for x in (0..self.get_width()).rev() {
+                    for y in 0..self.get_length() {
+                        self.move_tile(y, x, direction);
+                    }
+                }
+            }
+            Direction::Up => {
+                for y in 0..self.get_length() {
+                    for x in 0..self.get_width() {
+                        self.move_tile(y, x, direction);
+                    }
+                }
+            }
+            Direction::Down => {
+                for y in (0..self.get_length()).rev() {
+                    for x in 0..self.get_width() {
+                        self.move_tile(y, x, direction);
+                    }
+                }
+            }
+        };
+    }
+
+    fn move_tile(&mut self, y: usize, x: usize, direction: Direction) {
+        let (ty, tx) = match direction {
+            Direction::Left => (y, x.wrapping_sub(1)),  // (y, x - 1)
+            Direction::Right => (y, x.wrapping_add(1)), // (y, x + 1)
+            Direction::Up => (y.wrapping_sub(1), x),    // (y - 1, x)
+            Direction::Down => (y.wrapping_add(1), x),  // (y + 1, x)
+        };
+
+        let from = self.data.get((y, x)).unwrap();
+        let Some(to) = self.data.get((ty, tx)) else {
+            // Hit the wall
+            return;
+        };
+
+        // Target is regular or blocker tile: cannot move
+        // Origin is empty or blocker tile: cannot move
+        if let (Tile::Regular(_), Tile::Empty) = (from, to) {
+            self.data.swap((y, x), (ty, tx));
+        }
+    }
+
+    pub fn to_vec(&self) -> VecGrid {
+        VecGrid {
+            length: self.get_length(),
+            width: self.get_width(),
+            pattern: self
+                .data
+                .rows()
+                .into_iter()
+                .map(|row| row.iter().copied().collect())
+                .collect(),
+        }
+    }
+
+    pub fn to_ron(&self) -> String {
+        let pretty_config = PrettyConfig::new().depth_limit(2);
+        ron::ser::to_string_pretty(&self.to_vec(), pretty_config).unwrap()
+    }
+
+    pub fn from_vec(vec_grid: VecGrid) -> Result<Self, Box<dyn Error>> {
+        let vec_flat: Vec<Tile> = vec_grid.pattern.into_iter().flatten().collect();
+        let data = Array2::from_shape_vec((vec_grid.length, vec_grid.width), vec_flat)?;
+        Ok(Self { data })
+    }
+
+    pub fn from_ron(ron: &str) -> Result<Self, Box<dyn Error>> {
+        let vec: VecGrid = ron::de::from_str(ron)?;
+        Grid::from_vec(vec)
+    }
+}
