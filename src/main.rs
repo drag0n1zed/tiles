5eb55f792819f31d45a68d7dc6c7f2a749a5b24f
drag <drag0n1zed@outlook.com>
2026-01-19 16:10:39 +0800

    use ndarray crate, implement all cardinal directions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ndarray::prelude::*;
+
+#[derive(Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 #[derive(Clone, Copy, Debug)]
 enum Tile {
     Empty,
@@ -8,7 +18,7 @@ enum Tile {
 struct Grid {
     width: usize,
     length: usize,
-    data: Vec<Tile>,
+    data: Array2<Tile>,
     // data[x * length + width]. Upper left is [0, 0].
 }
 
@@ -17,21 +27,22 @@ impl Grid {
         Self {
             width,
             length,
-            data: vec![Tile::Empty; width * length],
+            data: Array2::from_elem((width, length), Tile::Empty),
         }
     }
-    fn index(&self, x: usize, y: usize) -> usize {
-        x * self.length + y
+
+    fn get(&self, x: usize, y: usize) -> Option<&Tile> {
+        self.data.get((x, y))
     }
 
-    fn get(&self, x: usize, y: usize) -> Tile {
-        let i = self.index(x, y);
-        self.data[i]
-    }
-
-    fn set(&mut self, x: usize, y: usize, tile: Tile) {
-        let i = self.index(x, y);
-        self.data[i] = tile;
+    fn set(&mut self, x: usize, y: usize, tile: Tile) -> bool {
+        match self.data.get_mut((x, y)) {
+            Some(val) => {
+                *val = tile;
+                true
+            }
+            None => false,
+        }
     }
 
     // Iterate order: iterate through every column from top to bottom, from left to right
@@ -40,39 +51,68 @@ impl Grid {
     // [2, 0] ... [2, l - 1],
     // ...
     // [w - 1, 0] ... [w - 1, l - 1],
-    fn move_grid_left(&mut self) {
-        for x in 0..self.width {
-            for y in 0..self.length {
-                self.move_tile_left(x, y);
+    fn move_grid(&mut self, direction: Direction) {
+        match direction {
+            Direction::Left => {
+                for x in 0..self.width {
+                    for y in 0..self.length {
+                        self.move_tile(x, y, direction);
+                    }
+                }
             }
-        }
+            Direction::Right => {
+                for x in (0..self.width).rev() {
+                    for y in 0..self.length {
+                        self.move_tile(x, y, direction);
+                    }
+                }
+            }
+            Direction::Up => {
+                for y in 0..self.length {
+                    for x in 0..self.width {
+                        self.move_tile(x, y, direction);
+                    }
+                }
+            }
+            Direction::Down => {
+                for y in (0..self.length).rev() {
+                    for x in 0..self.width {
+                        self.move_tile(x, y, direction);
+                    }
+                }
+            }
+        };
     }
 
-    fn move_tile_left(&mut self, x: usize, y: usize) {
-        if x == 0 {
+    fn move_tile(&mut self, x: usize, y: usize, direction: Direction) {
+        let (tx, ty) = match direction {
+            Direction::Left => (x.wrapping_sub(1), y),  // (x - 1, y)
+            Direction::Right => (x.wrapping_add(1), y), // (x + 1, y)
+            Direction::Up => (x, y.wrapping_sub(1)),    // (x, y + 1)
+            Direction::Down => (x, y.wrapping_add(1)),  // (x, y - 1)
+        };
+
+        let from = self.data.get((x, y)).unwrap();
+        let Some(to) = self.data.get((tx, ty)) else {
+            // Hit the wall
             return;
-        }
+        };
 
-        let from = self.index(x, y);
-        let to = self.index(x - 1, y);
-        let mut move_list: Vec<(usize, usize)> = Vec::new();
-
-        // Target is regular or blocker tile or the wall: cannot move
+        // Target is regular or blocker tile: cannot move
         // Origin is empty or blocker tile: cannot move
-        if let (Tile::Empty, Tile::Regular(_)) = (self.data[to], self.data[from]) {
-            move_list.push((from, to));
-        }
-
-        for (from, to) in move_list {
-            self.data.swap(from, to); // (Empty, Regular) -> (Regular, Empty)
+        if let (Tile::Regular(_), Tile::Empty) = (from, to) {
+            self.data.swap((x, y), (tx, ty));
         }
     }
 }
 
 fn main() {
-    let mut grid = Grid::new(5, 5);
-    grid.set(2, 0, Tile::Regular(0x11223344));
-    grid.set(1, 0, Tile::Blocker);
-    grid.move_grid_left();
-    println!("{:?}", grid.get(1, 0));
+    let mut grid = Grid::new(3, 3);
+    grid.set(2, 0, Tile::Regular(0));
+    grid.set(2, 1, Tile::Regular(1));
+
+    grid.move_grid(Direction::Down);
+    println!("{:?}", grid.get(2, 0).unwrap());
+    println!("{:?}", grid.get(2, 1).unwrap());
+    println!("{:?}", grid.get(2, 2).unwrap());
 }
